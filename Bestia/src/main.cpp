37a5a9268aa3d27c13c414e6e484035dc35f0b33
Mateusz Milczarczyk <matmilcz@gmail.com>
2020-04-24 21:53:46 +0200

    Create VS project solution

---
+++ Bestia/src/main.cpp
@@ -0,0 +1,29 @@
+#include <SFML/Graphics.hpp>
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+#ifdef _DEBUG
+    std::cout << "Here you can log things that will not appear in release mode:" << '\n';
+#endif // _DEBUG
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
+    return 0;
+}
