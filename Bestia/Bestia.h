cd937c0d5869dd58b4a5fb816004e969d47fd451
Mateusz Milczarczyk <matmilcz@gmail.com>
2020-05-04 22:45:56 +0200

    Set inline for resizeView fcn

--- Bestia/Bestia.h
+++ Bestia/Bestia.h
@@ -16,7 +16,7 @@ constexpr uint WINDOW_HEIGHT = 600;
 constexpr float VIEW_WIDTH = WINDOW_WIDTH;
 constexpr float VIEW_HEIGHT = WINDOW_HEIGHT;
 
-void resizeView(const sf::Window& window, sf::View& view)
+inline void resizeView(const sf::Window& window, sf::View& view)
 {
 	const float aspectRatio = static_cast<float>(window.getSize().x) / static_cast<float>(window.getSize().y);
 	view.setSize(VIEW_HEIGHT * aspectRatio, VIEW_HEIGHT);
