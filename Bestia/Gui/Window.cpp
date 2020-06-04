ddda5bd57d91c1cc3b481d9233b6115a675eca45
matmilcz <matmilcz@gmail.com>
2020-06-04 14:42:00 +0200

    Rename variable

--- Bestia/Gui/Window.cpp
+++ Bestia/Gui/Window.cpp
@@ -7,10 +7,10 @@ namespace gui {
     {
         m_renderWindow.setVerticalSyncEnabled(true);
 
-        constexpr uint offsetX = 14;
-        constexpr uint offsetY = 5;
+        constexpr uint hotspotX = 14;
+        constexpr uint hotspotY = 5;
         m_cursorImage.loadFromFile("Resources/sprites/cursor.png");
-        m_cursor.loadFromPixels(m_cursorImage.getPixelsPtr(), m_cursorImage.getSize(), sf::Vector2u{ offsetX, offsetY });
+        m_cursor.loadFromPixels(m_cursorImage.getPixelsPtr(), m_cursorImage.getSize(), sf::Vector2u{ hotspotX, hotspotY });
 
         m_renderWindow.setMouseCursor(m_cursor);
         m_renderWindow.setMouseCursorGrabbed(true);
