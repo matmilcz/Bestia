5b708274d8dc6d75636ec7e01648fa9eff42359d
matmilcz <matmilcz@gmail.com>
2020-09-21 18:07:38 +0200

    add timer

---
+++ Bestia/EventSystem/Timer.cpp
@@ -0,0 +1,41 @@
+#include "Timer.h"
+
+namespace bestia {
+namespace event {
+namespace timer {
+
+	Timer::Timer(const size_t& interval) : m_interval(std::chrono::milliseconds(interval))
+	{
+
+	}
+
+	Timer::~Timer()
+	{
+		m_timerThread.join();
+	}
+
+	void Timer::start()
+	{
+		m_isEnabled = true;
+
+		m_timerThread = std::thread([this]() {
+			std::unique_lock<std::mutex> uniqueLock(m_mutex);
+
+			while (!m_cv.wait_for(uniqueLock, m_interval, [this]() { return !m_isEnabled; }))
+			{
+				// TODO: send timeout event
+				std::this_thread::sleep_for(m_interval);
+			}
+			});
+	}
+
+	void Timer::stop()
+	{
+		std::lock_guard<std::mutex> guardLock(m_mutex);
+		m_isEnabled = false;
+		m_cv.notify_one();
+	}
+
+}
+}
+}
---
+++ Bestia/EventSystem/Timer.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <thread>
+#include <chrono>
+#include <condition_variable>
+#include <mutex>
+
+namespace bestia {
+namespace event {
+namespace timer {
+
+	constexpr size_t DEFAULT_INTERVAL = 1000;
+
+	class Timer
+	{
+	public:
+		Timer(const size_t& interval);
+		Timer(const Timer&) = delete;
+		Timer& operator= (const Timer&) = delete;
+		~Timer();
+
+		void start();
+		void stop();
+
+	private:
+		std::thread m_timerThread;
+		std::condition_variable m_cv;
+		std::mutex m_mutex;
+		std::chrono::milliseconds m_interval{ DEFAULT_INTERVAL };
+		bool m_isEnabled = false;
+	};
+
+}
+}
+}
