fbf6933f278b61ec8667ca5244f1e3bcc6fe956a
matmilcz <matmilcz@gmail.com>
2020-10-11 17:35:05 +0200

    replace old cursor with new one

--- Bestia/Gui/Window.cpp
+++ Bestia/Gui/Window.cpp
@@ -7,9 +7,9 @@ namespace gui {
     {
         m_renderWindow.setVerticalSyncEnabled(true);
 
-        constexpr uint hotspotX = 14;
-        constexpr uint hotspotY = 5;
-        m_cursorImage.loadFromFile("Resources/sprites/cursor.png");
+        constexpr uint hotspotX = 0;
+        constexpr uint hotspotY = 0;
+        m_cursorImage.loadFromFile("Resources/sprites/cursor/small_rounded_up_left_rescaled.png"); // TODO: save the pixel array of original image and rescale
         m_cursor.loadFromPixels(m_cursorImage.getPixelsPtr(), m_cursorImage.getSize(), sf::Vector2u{ hotspotX, hotspotY });
 
         m_renderWindow.setMouseCursor(m_cursor);
